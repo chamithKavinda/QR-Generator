401b26ce0ffabc1b539957f65c6dcf794aaf1a5e
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-05 13:38:34 +0530

    validation added

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ let qrImage = document.getElementById("qrImage");
 let qrText = document.getElementById("qrText");
 
 function generateQR(){
-    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    if(qrText.value.length > 0){
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+
+        imgBox.classList.add("show-img");
+    }
+    
 
 }
 
