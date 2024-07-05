7f91df43645e0fae1777b2c860f4627cf6536fce
chamithKavinda <chamith13kavinda@gmail.com>
2024-07-05 13:46:13 +0530

    content done and dusted

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ function generateQR(){
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
 
         imgBox.classList.add("show-img");
+    }else{
+        qrText.classList.add('error');
+        setTimeout(()=>{
+            qrText.classList.remove('error');
+        },500);
     }
     
 
